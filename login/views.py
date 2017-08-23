efb710722395f04f649a5a26e50df0b537ad4ec0
Aifear <kimolav@yandex.ru>
2017-08-23 22:50:15 +0300

    Login

--- login/views.py
+++ login/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.http import HttpResponse
 from django.contrib import auth
-from django.core.context_processors import csrf
 
 def login(request):
     args={}
-    args.update(csrf(request))
     if request.POST:
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
