0f20be2e6072dec4d59d848835ce7b6f1986ebad
Aifear <kimolav@yandex.ru>
2017-08-24 00:27:56 +0300

    Login

--- login/views.py
+++ login/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.http import HttpResponse
 from django.contrib import auth
+from django.template.context_processors import csrf
 
 def login(request):
     args={}
+    args.update(csrf(request))
     if request.POST:
-        return HttpResponse('123')
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
         print (username, password)
         user = auth.authenticate (username=username, password=password)
         if user is not None:
-            auth.Login(request, user)
+            auth.login(request, user)
             return redirect('/')
         else:
             args['login_error'] = 'User is not defined'
-            return render_to_response('login/login.html', args)
+            return render(request, 'login/login.html', args)
 
     else:
-        return redirect('/admin')
-        return render_to_response('login/login.html', args)
+        return render(request, 'login/login.html', args)
 
 
 def logout(request):
