ecee868085a7b67f73c30c93fc90ca7bfb8bbac0
Aifear <kimolav@yandex.ru>
2017-08-24 03:04:28 +0300

    Admin panel

--- landing/views.py
+++ landing/views.py
@@ -6,7 +6,9 @@ from django.contrib import auth
 
 def landing(request):
     Articles = Article.objects.all()
-    return render(request, 'landing/index.html', {'Articles':Articles, 'username':auth.get_user(request).username})
+    return render(request, 'landing/index.html', {'Articles':Articles,
+                                                  'username':auth.get_user(request).username,
+                                                  'user':auth.get_user(request)})
 
 def single(request, question_id):
     article = get_object_or_404(Article, pk=question_id)
