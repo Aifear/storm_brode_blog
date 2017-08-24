0e1dcf1865d803e3ac4e9c6346fce2d65bcbb373
Aifear <danila_vit@tut.by>
2017-08-24 21:39:01 +0400

    Get ip

--- ipconfig/views.py
+++ ipconfig/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def show(request):
-    return HttpResponse(' '.join(request.META))
+    values = request.META.items()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
