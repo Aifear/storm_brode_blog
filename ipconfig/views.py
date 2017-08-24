8e54e43b38de138e0f17cb673f3c584763baf9d3
Aifear <danila_vit@tut.by>
2017-08-24 21:48:47 +0400

    Get ip

--- ipconfig/views.py
+++ ipconfig/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def show(request):
-    values = request.META.items()
-    html = []
-    for k, v in values:
-        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
-    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+    ip = request.META['REMOTE_ADDR']
+    return HttpResponse('Your ip is: {}'.format(ip))
