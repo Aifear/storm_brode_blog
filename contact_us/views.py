2cb5ebe847290404cae67d0cf7a6051ee9203262
Aifear <kimolav@yandex.ru>
2017-08-24 04:35:37 +0300

    Contact us

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,41 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.core.mail import send_mail, BadHeaderError, mail_managers
 from .models import ContactForm
+from django.template.context_processors import csrf
 import smtplib
 # Create your views he
 
-
-"""def contactView(request):
-	if request.method == 'POST':
-		form = ContactForm(request.POST)
-		#Если форма заполнена корректно, сохраняем все введённые пользователем значения
-		if form.is_valid():
-			subject = form.cleaned_data['subject']
-			sender = form.cleaned_data['sender']
-			message = form.cleaned_data['message']
-			copy = form.cleaned_data['copy']
-
-			recipients = ['kimolav@yandex.ru']
-			#Если пользователь захотел получить копию себе, добавляем его в список получателей
-			if copy:
-				recipients.append(sender)
-			try:
-				send_mail(subject, message, 'smit.chan.you@gmail.com', recipients, fail_silently=False)
-			except BadHeaderError: #Защита от уязвимости
-				return HttpResponse('Invalid header found')
-			#Переходим на другую страницу, если сообщение отправлено
-			return render(request, 'contact_us/thanks.html')
-	else:
-		#Заполняем форму
-		form = ContactForm()
-	#Отправляем форму на страницу
-	return render(request, 'contact_us/index.html', {'form': form})"""
-
 def contactView(request):
-    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-    smtpObj.starttls()
-    smtpObj.login('smit.chan.you@gmail.com', 'SloniKsV79')
-    smtpObj.sendmail("justkiddingboat@gmail.com", "kimolav@yandex.ru", "go to bed!")
-    smtpObj.quit()
+    args ={}
+    args.update(csrf(request))
+    if request.POST:
+        subject = request.POST.get('subject','')
+        message = request.POST.get('message','')
+        from_email = request.POST.get('email','')
+        if subject and message and from_email:
+            try:
+                send_mail(subject,
+                          message,
+                          from_email,
+                          ['kimolav@yandex.ru'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('/')
+
+        else:
+            return HttpResponse('Make shure')
+    else:
+        return render(request, 'contact_us/index.html', args)
+
