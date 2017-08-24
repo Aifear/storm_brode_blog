2168ce584c3dba41a022bd018525e7a6294c9df9
Aifear <danila_vit@tut.by>
2017-08-24 22:07:11 +0400

    Get ip

--- ipconfig/views.py
+++ ipconfig/views.py
@@ -6,6 +6,6 @@ def show(request):
     html = []
     for k, v in values:
         html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
-        if v=='HTTP_X_REAL_IP':
+        if k=='HTTP_X_REAL_IP':
             return HttpResponse('Your ip is: %s' % (v))
     return HttpResponse('<table>%s</table>' % '\n'.join(html))
